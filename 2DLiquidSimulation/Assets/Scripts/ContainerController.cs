87563463eb30590b6393c62b65bc87949c1734df
LittleHedgehogM <1041495644good@gmail.com>
2025-02-12 20:19:01 +0800

    简单PouringEffect

--- 2DLiquidSimulation/Assets/Scripts/ContainerController.cs
+++ 2DLiquidSimulation/Assets/Scripts/ContainerController.cs
@@ -6,5 +6,16 @@ public class ContainerController : MonoBehaviour
 {
     [SerializeField] private Transform LiquidContainer;
     [SerializeField] private Collider2D myCollider;
+    [SerializeField] private Transform liquid;
+    [SerializeField] private Transform receiverContainer;
 
+    private void pouringStart()
+    {
+        liquid.SetParent(LiquidContainer);
+    }
+
+    public void pouringEnd()
+    {
+        liquid.SetParent(receiverContainer);
+    }
 }
