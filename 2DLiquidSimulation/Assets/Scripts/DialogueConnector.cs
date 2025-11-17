60ba55c196bbbff232db1a389df2fe86d251f119
LittleHedgehogM <1041495644good@gmail.com>
2025-11-17 09:33:10 +0800

    Some Updates

---
+++ 2DLiquidSimulation/Assets/Scripts/DialogueConnector.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+public class DialogueConnector : MonoBehaviour
+{
+    [YarnCommand("IncrementQuestionScore")]
+    public void IncrementQuestionScore()
+    {
+    
+    }
+
+    [YarnCommand("DecrementQuestionScore")]
+    public void DecrementQuestionScore()
+    {
+
+    }
+
+    [YarnCommand("IncrementInteractionScore")]
+    public void IncrementInteractionScore()
+    {
+
+    }
+
+    [YarnCommand("DecrementInteractionScore")]
+    public void DecrementInteractionScore()
+    {
+        
+    }
+
+}
